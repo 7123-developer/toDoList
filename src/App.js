58cb83def526ef12a443065ca4cdc4607775d59b
7123-developer <nikhiltiwari7123@gmail.com>
2021-04-17 16:30:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import "./App.css";
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import FontAwesome from "react-fontawesome";
 
 function App() {
   const [toDoData, setToDoData] = useState([]);
-  const [userInput, setUserInput] = useState({});
-  const [updateInput, setUpdateInput] = useState("")
-  const [update, setUpdate] = useState(false)
+  const [userInput, setUserInput] = useState(null);
+  const [updateInput, setUpdateInput] = useState("");
+  const [update, setUpdate] = useState(false);
   const onchangeInput = (e) => {
     const { value } = e.target;
     const data = {
       name: value,
       id: Math.random(),
+      done: 0,
     };
     setUserInput(data);
   };
   const onSaveData = () => {
     setToDoData([...toDoData, userInput]);
-    setUserInput({})
+    setUserInput(null);
   };
+
   const onUpdateData = (data) => {
     if (updateInput) {
       const updatetodo = toDoData.map((item) => {
@@ -27,54 +32,82 @@ function App() {
         }
         return item;
       });
-    setToDoData(updatetodo);
-    setUpdateInput("")
-    setUpdate(false)
+      setToDoData(updatetodo);
+      setUpdateInput("");
+      setUpdate(false);
     } else {
-      setUpdate(true)
-    } 
+      setUpdate(true);
+    }
   };
   const onDelete = (data) => {
     const updatetodo = toDoData.filter((item) => item.id !== data.id);
     setToDoData(updatetodo);
   };
   const onUpdateInput = (e) => {
+    const { value } = e.target;
+    setUpdateInput(value);
+  };
+  const onDoneTask = (e,data) => {
     const {value} = e.target
-    setUpdateInput(value)
+    const updatetodo = toDoData.map((item) => {
+      if (item.id === data.id) {
+        item.done = !data.done;
+        return item;
+      }
+      return item;
+    });
+    setToDoData(updatetodo);
   }
   return (
     <div className="App">
-      <input
-        name="data"
-        placeholder="To Do Item"
-        onChange={(e) => onchangeInput(e)}
-      />
-      <button onClick={() => onSaveData()}>save </button>
+      <div className="main-heading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="main-input-container">
+        <input
+          name="data"
+          placeholder="To Do Item"
+          onChange={(e) => onchangeInput(e)}
+        />
+        <Button color="primary" onClick={() => onSaveData()}>
+          save{" "}
+        </Button>
+      </div>
       {toDoData.length
         ? toDoData.map((item) => (
             <div className="todo-data-container">
-              {update ?<input defaultValue={item.name} onChange={(e)=> onUpdateInput(e)} /> :<div onClick={() => onUpdateData(item)}>{item.name}</div>}
-              <div className="button-container">
-              <button onClick={() => onUpdateData(item)}>update</button>
-              <button onClick={() => onDelete(item)}>Delete</button>
+        <div className="todo-data-wraper">
+              <div >
+                {update ? (
+                  <div className="main-input-container">
+                    <input
+                      defaultValue={item.name}
+                      onChange={(e) => onUpdateInput(e)}
+                    />
+                    <Button color="primary" onClick={() => onUpdateData(item)}>
+                      Update{" "}
+                    </Button>
+                  </div>
+                ) : (
+                  <div className="checkbox-text">
+                    <input type="checkbox" onClick={(e)=>onDoneTask(e,item)} />
+                    <div className={item.done ? "compleat-task" : null}>
+                      {item.name}
+                    </div>
+                  </div>
+                )}
               </div>
+
+              {update ? null : (
+                <div className="button-container">
+                  <FontAwesome name="edit" onClick={() => onUpdateData(item)} />
+                  <FontAwesome name="trash" onClick={() => onDelete(item)} />
+                </div>
+              )}
+            </div>
             </div>
           ))
-        : null}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+        : <div className="no-task">No task ToDo!</div>}
     </div>
   );
 }
